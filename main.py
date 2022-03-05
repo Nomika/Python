d4daaddc6bdab57180824686d34be1a06da449c2
Nomika <nomika1@op.pl>
2022-03-05 11:54:20 +0100

    Zadanie 14

---
+++ main.py
@@ -0,0 +1,16 @@
+
+x = 3
+if x > 2:
+    print("Gratulacje!")
+    print("Warunek spelniony")
+else:
+    print("Niestety...")
+    print("Warinek nie jest spełniony")
+
+print("Koniec programu")
+
+
+
+
+
+
